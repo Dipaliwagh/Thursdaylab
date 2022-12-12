8c8dbd3e188a7bba93a207e98d79bf4eeed51c0c
Dipali Waghmare <117341163+Dipaliwagh@users.noreply.github.com>
2022-12-12 13:55:13 +0530

    Add files via upload

--- Project/ShoppingKart.java
+++ Project/ShoppingKart.java
@@ -1,5 +1,18 @@
-1
-  2
-  3
-  4
-  5
+package shoppingKartusingarray;
+
+import java.util.Scanner;
+
+//This is main entry point
+
+public class ShoppingKart 
+{
+	  static Scanner sc=new Scanner(System.in); 
+	    
+	    
+			public static void main(String[] args) {
+				System.out.println("\t\t~~Welcome To Shopping Kart~~");
+				ShoopingKartOperations.AllMenus();
+			}
+	}
+
+
